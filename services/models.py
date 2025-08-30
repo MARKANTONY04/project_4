b1b768b1f7e0a3cd0a299aabd0df9a31f0a1f9e7
MARKANTONY04 <markmarsden04@gmail.com>
2025-08-30 14:02:34 +0100

    created models for services app

--- services/models.py
+++ services/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class GymSubscription(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    duration = models.IntegerField(help_text="Duration in months")
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    schedule = models.CharField(max_length=200)
+    instructor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class NutritionGuide(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    duration = models.IntegerField(help_text="Duration in hours")
+
+    def __str__(self):
+        return self.title
