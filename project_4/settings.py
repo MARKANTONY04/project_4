52772abbde76b42fd5a91a0fe0f27a1754e77577
MARKANTONY04 <markmarsden04@gmail.com>
2025-09-02 20:55:22 +0100

    added procfile and added heroku to allowed host

--- project_4/settings.py
+++ project_4/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$dlpi$#79mm6w=$zjb1!d7k$ur8n&30-)_44vrw2q0la_o(__k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
