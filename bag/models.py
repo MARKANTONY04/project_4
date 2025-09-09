f3c7d9abb8601bcbc3859be6d3e7521e2bea8572
MARKANTONY04 <markmarsden04@gmail.com>
2025-09-09 11:59:01 +0100

    created model for bag

--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from services.models import GymSubscription, FitnessClass, NutritionGuide
 
-# Create your models here.
+# bag model
+
+class SavedBagItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="saved_bag")
+    content_type = models.CharField(max_length=20, choices=[
+        ('subscription', 'Subscription'),
+        ('class', 'Class'),
+        ('guide', 'Nutrition Guide'),
+    ])
+    object_id = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s item ({self.content_type} #{self.object_id})"
--- project_4/settings.py
+++ project_4/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     # web apps
     'home',
     'services',
+    'bag',
 ]
 
 MIDDLEWARE = [
