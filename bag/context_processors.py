ad72187040e31443a1a18a895cf9ea53c229cb01
MARKANTONY04 <markmarsden04@gmail.com>
2025-09-09 17:36:41 +0100

    updated context processor and navbar to include bag item count

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -1,8 +1,19 @@
-from .bag_utils import get_bag_items
-
-# bag context processor
+from .models import SavedBagItem
 
 def bag_contents(request):
+    bag_items = []
+    bag_total_quantity = 0
+
+    if request.user.is_authenticated:
+        # Logged-in user: fetch saved items
+        saved_items = SavedBagItem.objects.filter(user=request.user)
+        bag_items = saved_items
+        bag_total_quantity = sum(item.quantity for item in saved_items)
+    else:
+        # Guest user: check session
+        session_bag = request.session.get("bag", {})
+        bag_total_quantity = sum(item["quantity"] for item in session_bag.values())
+
     return {
-        'bag_items': get_bag_items(request),
+        "bag_total_quantity": bag_total_quantity,
     }
