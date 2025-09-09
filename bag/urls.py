eadb14c8d6904a4e3bd09f797b86ebc7312201df
MARKANTONY04 <markmarsden04@gmail.com>
2025-09-09 20:23:03 +0100

    updated bag urls to fit with template

--- bag/urls.py
+++ bag/urls.py
@@ -5,7 +5,7 @@ app_name = "bag"
 
 urlpatterns = [
     path("", views.view_bag, name="view_bag"),
-    path("add/<str:product_type>/<int:product_id>/", views.add_to_bag, name="add_to_bag"),
-    path("update/<str:product_type>/<int:product_id>/", views.update_bag, name="update_bag"),
-    path("remove/<str:product_type>/<int:product_id>/", views.remove_from_bag, name="remove_from_bag"),
+    path("add/<str:item_type>/<int:item_id>/", views.add_to_bag, name="add_to_bag"),
+    path("update/<str:item_type>/<int:item_id>/", views.update_bag, name="update_bag"),
+    path("remove/<str:item_type>/<int:item_id>/", views.remove_from_bag, name="remove_from_bag"),
 ]
