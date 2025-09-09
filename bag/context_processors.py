4237e538e3131945b4d71a0fb5bd5ab5c73d677f
MARKANTONY04 <markmarsden04@gmail.com>
2025-09-09 20:00:52 +0100

    updated context processor to fix display error and created testy.html to test templates

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -1,19 +1,60 @@
 from .models import SavedBagItem
+from services.models import GymSubscription, FitnessClass, NutritionGuide
 
 def bag_contents(request):
     bag_items = []
     bag_total_quantity = 0
 
     if request.user.is_authenticated:
-        # Logged-in user: fetch saved items
+        # Logged-in user: fetch saved items from DB
         saved_items = SavedBagItem.objects.filter(user=request.user)
-        bag_items = saved_items
-        bag_total_quantity = sum(item.quantity for item in saved_items)
+        for item in saved_items:
+            if item.item_type == "subscription":
+                product = GymSubscription.objects.filter(pk=item.item_id).first()
+            elif item.item_type == "class":
+                product = FitnessClass.objects.filter(pk=item.item_id).first()
+            elif item.item_type == "guide":
+                product = NutritionGuide.objects.filter(pk=item.item_id).first()
+            else:
+                product = None
+
+            if product:
+                bag_items.append({
+                    "type": item.item_type,
+                    "id": item.item_id,
+                    "product": product,
+                    "quantity": item.quantity,
+                })
+                bag_total_quantity += item.quantity
+
     else:
-        # Guest user: check session
+        # Guest user: fetch from session
         session_bag = request.session.get("bag", {})
-        bag_total_quantity = sum(item["quantity"] for item in session_bag.values())
+        for key, item in session_bag.items():
+            item_type = item.get("type")
+            item_id = item.get("id")
+            quantity = item.get("quantity", 0)
+
+            if item_type == "subscription":
+                product = GymSubscription.objects.filter(pk=item_id).first()
+            elif item_type == "class":
+                product = FitnessClass.objects.filter(pk=item_id).first()
+            elif item_type == "guide":
+                product = NutritionGuide.objects.filter(pk=item_id).first()
+            else:
+                product = None
+
+            if product:
+                bag_items.append({
+                    "type": item_type,
+                    "id": item_id,
+                    "product": product,
+                    "quantity": quantity,
+                })
+                bag_total_quantity += quantity
 
     return {
+        "bag_items": bag_items,
         "bag_total_quantity": bag_total_quantity,
     }
+
