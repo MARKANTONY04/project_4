206c8dc0c93d8ad2375c1e428ca03fbeffbb0f6d
MARKANTONY04 <markmarsden04@gmail.com>
2025-09-09 18:08:01 +0100

    added update url to bag app

--- bag/urls.py
+++ bag/urls.py
@@ -5,6 +5,7 @@ app_name = "bag"
 
 urlpatterns = [
     path("", views.view_bag, name="view_bag"),
-    path("add/<str:content_type>/<int:object_id>/", views.add_to_bag, name="add_to_bag"),
-    path("remove/<str:content_type>/<int:object_id>/", views.remove_from_bag, name="remove_from_bag"),
+    path("add/<str:product_type>/<int:product_id>/", views.add_to_bag, name="add_to_bag"),
+    path("update/<str:product_type>/<int:product_id>/", views.update_bag, name="update_bag"),
+    path("remove/<str:product_type>/<int:product_id>/", views.remove_from_bag, name="remove_from_bag"),
 ]
