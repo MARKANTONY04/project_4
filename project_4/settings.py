5451031bffbb5c19d86389626038a45129f68893
MARKANTONY04 <markmarsden04@gmail.com>
2025-09-16 13:13:49 +0100

    added context processor for stripe in checkout

--- project_4/settings.py
+++ project_4/settings.py
@@ -88,6 +88,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.context_processors.bag_contents', # bag context processor
+                'checkout.context_processors.stripe_publishable_key',
             ],
         },
     },
