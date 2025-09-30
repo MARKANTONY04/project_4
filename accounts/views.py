399d855b1d5b96ea913a796a8da49ba3b55f45f7
MARKANTONY04 <markmarsden04@gmail.com>
2025-09-30 09:51:46 +0100

    added account views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+#logged in users can change/update name
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm
+
+@login_required
+def update_profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_updated')  
+    else:
+        form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'accounts/update_profile.html', {'form': form})
