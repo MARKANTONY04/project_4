94b2ae12af67cb7a4f391af45382e99d8c44c24f
MARKANTONY04 <markmarsden04@gmail.com>
2026-01-09 16:31:41 +0000

    added updated class, guide, and dashboard.html templates with urls.py  to allow CRUD functionality across guides and classes too

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,23 +1,26 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path("", views.admin_dashboard, name="admin_dashboard"),
 
-    # LIST PAGES
-    path("subscriptions/", views.subscription_list, name="admin_service_list"),
-    path("classes/", views.class_list, name="admin_class_list"),
-    path("guides/", views.guide_list, name="admin_guide_list"),
+    # Lists
+    path("subscriptions/", views.subscription_list, name="subscription_list"),
+    path("classes/", views.class_list, name="class_list"),
+    path("guides/", views.guide_list, name="guide_list"),
 
-    # CREATE / UPDATE / DELETE
+    # Subscriptions
     path("subscription/add/", views.add_subscription, name="add_subscription"),
     path("subscription/edit/<int:pk>/", views.edit_subscription, name="edit_subscription"),
     path("subscription/delete/<int:pk>/", views.delete_subscription, name="delete_subscription"),
 
+    # Classes
     path("class/add/", views.add_class, name="add_class"),
     path("class/edit/<int:pk>/", views.edit_class, name="edit_class"),
     path("class/delete/<int:pk>/", views.delete_class, name="delete_class"),
 
+    # Guides
     path("guide/add/", views.add_guide, name="add_guide"),
     path("guide/edit/<int:pk>/", views.edit_guide, name="edit_guide"),
     path("guide/delete/<int:pk>/", views.delete_guide, name="delete_guide"),
